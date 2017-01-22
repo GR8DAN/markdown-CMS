95173e39f918d191ee027d97fcecd6e36219f2ba
Daniel S. Fowler <danfowler@sky.com>
2017-01-22 23:02:21 +0000

    Any case on comments enabler
    The meta data to enable comments can be in any case. Basically an value
    other than "no" (in any case).

--- md/md-comment.php
+++ md/md-comment.php
@@ -3,7 +3,10 @@
     if(isset($MD_SETTINGS['HOME'])) {
         //see if article has enabled comments
         if(array_key_exists('comments',$md_meta)) {
-            if($md_meta['comments'] != "no") {
+            if(empty($md_meta["comments"])) {
+                $md_meta["comments"]="no";
+            }
+            if(strtolower($md_meta['comments']) != "no") {
                 //page url for identity for Disqus
                 $page_url=rtrim($MD_SETTINGS['HOME'],"/").Md_BrowserRequest();
                 //Get the comment code and add the site settings
